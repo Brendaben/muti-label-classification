b458bd539885e6e8d3ab08755fd598434a066aa8
Truong Phuc Anh <truongphucanh.main@gmail.com>
2017-12-16 21:42:45 +0700

    feat: Add script to run on all data sets

---
+++ src/run.cmd
@@ -0,0 +1,15 @@
+@echo off
+
+::1. Run on random generated data.
+main.py
+
+::2. Run on yeast data
+main.py yeast
+
+::3. Run on birds data
+main.py birds
+
+::4. Run on bibtex data
+main.py bibtex
+
+pause
